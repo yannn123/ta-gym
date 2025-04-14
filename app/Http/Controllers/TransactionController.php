235c07fe2f4e34744f931599d3b58ffa46d72057
ImGoziee <ghozimalang87@gmail.com>
2025-04-14 18:30:08 +0700

    update controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,8 +5,26 @@ use Illuminate\Http\Request;
 
 class TransactionController extends Controller 
 {
-    public function showTransaction()
+    public function showTransaction(Request $request)
     {
-        return view('Transaction.index');
+        $idLayanan = $request->input('id_layanan', 1);
+        $membershipType = $request->input('membership_type', 'All You Can Fit Membership');
+        $duration = $request->input('duration', '1 Bulan');
+        $price = $request->input('price', '85000');
+        
+        $formattedPrice = number_format($price, 0, ',', '.');
+        
+        return view('transaction.index', [
+            'idLayanan' => $idLayanan,
+            'membershipType' => $membershipType,
+            'duration' => $duration,
+            'price' => $price,
+            'formattedPrice' => $formattedPrice
+        ]);
+    }
+
+    public function processTransaction(Request $request)
+    {
+        
     }
 }
